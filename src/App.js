687c9ff0c7f764b7253de9180be5742cb0924e79
Your Name <you@example.com>
2023-03-07 21:36:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import { Box, Button, FormControl, FormLabel, Heading, Input, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Textarea
+} from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
@@ -6,7 +15,7 @@ import "./stylesheet.css";
 
 function App() {
   // Define your events data here
-  const events = [
+  const [events, setEvents] = useState([
     {
       name: "Event 1",
       description: "This is event 1",
@@ -19,14 +28,20 @@ function App() {
       location: "Location 2",
       date: "2023-03-09"
     }
-  ];
+  ]);
+
+  // Callback function to add a new event to the events array
+  const handleCreateEvent = (newEvent) => {
+    setEvents((prevEvents) => [...prevEvents, newEvent]);
+  };
 
   return (
     <ChakraProvider>
       <Router>
         <Routes>
           <Route path="/home" element={<HomePage />} />
-          <Route path="/create" element={<CreateEventPage />} /> 
+          {/* Pass the handleCreateEvent callback as a prop */}
+          <Route path="/create" element={<CreateEventPage onCreateEvent={handleCreateEvent} />} /> 
           {/* Pass the events data as props to the EventList component */}
           <Route path="/myevent" element={<EventList events={events} />} />
         </Routes>
@@ -60,7 +75,7 @@ function EventList({ events }) {
       bg="white"
     >
       <Heading as="h2" size="md" color="gray.800" mb={4}>
-        Events Today
+        กิจกรรมของฉัน
       </Heading>
       <Box>
         {events.map((event, index) => (
@@ -77,13 +92,13 @@ function EventList({ events }) {
               {event.name}
             </Heading>
             <Box mb={2}>
-              <b>Description: </b> {event.description}
+              <b>รายละเอียด: </b> {event.description}
             </Box>
             <Box mb={2}>
-              <b>Location: </b> {event.location}
+              <b>สถานที่: </b> {event.location}
             </Box>
             <Box mb={2}>
-              <b>Date: </b> {event.date}
+              <b>วันที่: </b> {event.date}
             </Box>
           </Box>
         ))}
@@ -93,42 +108,95 @@ function EventList({ events }) {
 }
 
 
-function CreateEventPage() {
-  return (
-    <Box
-      maxW="xl"
-      mx="auto"
-      m={5}
-      p={6}
-      borderWidth={1}
-      borderRadius={12}
-      boxShadow="md"
-      bg="white"
-    >
-      <Heading as="h1" size="lg" color="gray.800" mb={6}>
-        สร้างกิจกรรมใหม่
-      </Heading>
-      <FormControl id="eventName" mb={4}>
-        <FormLabel>ชื่อกิจกรรม</FormLabel>
-        <Input type="text" placeholder="กรอกชื่อกิจกรรม" />
-      </FormControl>
-      <FormControl id="eventDescription" mb={4}>
-        <FormLabel>รายละเอียด</FormLabel>
-        <Textarea placeholder="กรอกรายละเอียด" />
-      </FormControl>
-      <FormControl id="eventLocation" mb={4}>
-        <FormLabel>สถานที่ (ไม่บังคับ)</FormLabel>
-        <Input type="text" placeholder="กรอกสถานที่" />
-      </FormControl>
-      <FormControl id="eventDate" mb={6}>
-        <FormLabel>วันที่</FormLabel>
-        <Input type="date" />
-      </FormControl>
-      <Button colorScheme="blue" size="md" w="full" mt={2}>
-        Create Event
-      </Button>
-    </Box>
-  );
+function CreateEventPage({ onCreateEvent }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+  e.preventDefault(); 
+
+// Create a new event object
+const newEvent = {
+  name,
+  description,
+  location,
+  date
+};
+
+// Call the onCreateEvent callback with the new event object
+onCreateEvent(newEvent);
+
+// Clear the form fields
+setName("");
+setDescription("");
+setLocation("");
+setDate("");
+
+};
+
+return (
+<Box
+   maxW="xl"
+   mx="auto"
+   m={5}
+   p={6}
+   borderWidth={1}
+   borderRadius={12}
+   borderColor="gray.200"
+   bg="white"
+ >
+<Heading as="h2" size="md" color="gray.800" mb={4}>
+เพิ่มกิจกรรมใหม่
+</Heading>
+<form onSubmit={handleSubmit}>
+<FormControl id="name" mb={4}>
+<FormLabel>ชื่อกิจกรรม</FormLabel>
+<Input
+type="text"
+placeholder="กรอกชื่อกิจกรรม"
+value={name}
+onChange={(e) => setName(e.target.value)}
+required
+/>
+</FormControl>
+<FormControl id="description" mb={4}>
+<FormLabel>รายละเอียด</FormLabel>
+<Textarea
+placeholder="กรอกรายละเอียด"
+value={description}
+onChange={(e) => setDescription(e.target.value)}
+required
+/>
+</FormControl>
+<FormControl id="location" mb={4}>
+<FormLabel>สถานที่</FormLabel>
+<Input
+type="text"
+placeholder="กรอกสถานที่"
+value={location}
+onChange={(e) => setLocation(e.target.value)}
+required
+/>
+</FormControl>
+<FormControl id="date" mb={4}>
+<FormLabel>วันที่</FormLabel>
+<Input
+type="date"
+placeholder="Enter event date"
+value={date}
+onChange={(e) => setDate(e.target.value)}
+required
+/>
+</FormControl>
+<Button type="submit" colorScheme="blue" w="100%">
+ยืนยัน
+</Button>
+</form>
+</Box>
+);
 }
 
 export default App;

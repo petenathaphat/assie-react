e2ff9c7162bea8ca3bd7d2655349adc958c7692b
Your Name <you@example.com>
2023-03-07 11:37:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import { Box, Button, FormControl, FormLabel, Heading, Input, Textarea } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import "./stylesheet.css";
 
 function App() {
   return (
     <ChakraProvider>
-      <Box bg="url('/src/assie-bg-new.jpg') no-repeat center center fixed" bgSize="cover" >
-        <CreateEventPage />
-      </Box>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/create" element={<CreateEventPage />} /> />
+        </Routes>
+      </Router>
     </ChakraProvider>
+  );
+}
+
+function HomePage() {
+  return (
+    <Box bg="url('/src/assie-bg-new.jpg') no-repeat center center fixed" bgSize="cover">
+      {/* Add your homepage content here */}
+      <Box>
+        <Link to="/create">Create Event</Link>
+      </Box>
+    </Box>
   );
 }
 
@@ -24,7 +39,9 @@ function CreateEventPage() {
       boxShadow="md"
       bg="white"
     >
-      <Heading as="h1" size="lg" color="gray.800" mb={6}>สร้างกิจกรรมใหม่</Heading>
+      <Heading as="h1" size="lg" color="gray.800" mb={6}>
+        สร้างกิจกรรมใหม่
+      </Heading>
       <FormControl id="eventName" mb={4}>
         <FormLabel>ชื่อกิจกรรม</FormLabel>
         <Input type="text" placeholder="กรอกชื่อกิจกรรม" />
@@ -41,7 +58,9 @@ function CreateEventPage() {
         <FormLabel>วันที่</FormLabel>
         <Input type="date" />
       </FormControl>
-      <Button colorScheme="blue" size="md" w="full" mt={2}>Create Event</Button>
+      <Button colorScheme="blue" size="md" w="full" mt={2}>
+        Create Event
+      </Button>
     </Box>
   );
 }
